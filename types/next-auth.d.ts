4650c762f836942f6b9a2a69feb2b8473318443a
Ubuntu <indriputri@mobilsiap-wp.neo.internal>
2026-08-04 07:01:59 +0700

    feat(auth): setup NextAuth and Prisma

---
+++ types/next-auth.d.ts
@@ -0,0 +1,20 @@
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: DefaultSession["user"] & {
+      id: string;
+      role: string;
+    };
+  }
+
+  interface User {
+    role: string;
+  }
+}
+
+declare module "@auth/core/jwt" {
+  interface JWT {
+    role: string;
+  }
+}
